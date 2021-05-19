66490842f97e523cbd30181fc9a12fe3d7d9a9db
Adlai Holler <adlai@google.com>
2021-05-19 20:04:47 +0000

    RMX1921: Remove redundant build property for reduceOpsTaskSplitting This flag is true for everyone now. No need for it here. Change has no effect.
    Bug: 183612348
    Change-Id: Ib862937e7d441f4a21c74da4f38ee9b9749b7a52
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -129,7 +129,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
     ro.opengles.version=196610 \
     debug.renderengine.backend=skiaglthreaded \
-    renderthread.skia.reduceopstasksplitting=true \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno
 
